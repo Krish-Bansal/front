2719ad691dabf431f52d648d115378e7aa87ce1f
Krish-Bansal <skbansal907@gmail.com>
2023-05-24 17:44:34 +0545

    changes

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,4 +1,4 @@
-export const base_url = "https://defy-lifestyle.onrender.com/"
+export const base_url = "https://defy-lifestyle.onrender.com/api/"
 const getTokenFromLocalStorage = localStorage.getItem("customer") ? JSON.parse(localStorage.getItem("customer")) : null;
 
 export const config = {
