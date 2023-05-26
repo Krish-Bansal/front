e597b5456c40f2a53d36058f8712e20f05aee023
Krish-Bansal <skbansal907@gmail.com>
2023-05-26 17:06:03 +0545

    changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -86,106 +86,129 @@ const Footer = () => {
   // };
   return (
     <>
-      <footer className='py-2 footer-middle pt-5'>
+      <footer className='footer-middle'>
         <div className="container-xxl">
           <div className="row">
-            <div className="col-12 col-md-6 contact-mobile">
-              <h4 className="mb-3" onClick={handleContactToggle}>
-                Contact Us
-              </h4>
-              {isContactOpen && (
-                <div className="contact-info">
-                  <p>
+            <div className="col-12 col-md-6">
+              <div class="footer-link flex flex-col">
+                <h4 class="mb-[1%]" onClick={handleContactToggle}>
+                  Contact Us
+                </h4>
+                <div class={isContactOpen ? "link-container open" : "link-container"}>
+                  <div class="link-item">
                     <span className="">Address:</span>
                     <address className="mb-2">
                       National Business Trade Center (NBTC), Khasibazar, 1<sup>st</sup> Floor, Shop No. 4138, Kathmandu, Nepal
                     </address>
+                  </div>
+                  <div class="link-item">
                     <span>Call:</span>
                     <a href="tel:+9779813783811" className="mb-1 underline">
                       +9779813783811
                     </a>
-                    <div>
-                      <span>Email:</span>
-                      <a href="mailto:skbansal907@gmail.com" className="mt-2 mb-3 underline">
-                        skbansal907@gmail.com
-                      </a>
-                    </div>
-                    <div className="social-icons fs-2">
-                      <a href="https://www.instagram.com/defy.lifestyle/?hl=en">
-                        <IoLogoInstagram />
-                      </a>
-                      <a href="https://www.facebook.com/defylifestyle/">
-                        <IoLogoFacebook />
-                      </a>
-                    </div>
-                  </p>
+                  </div>
+                  <div class="link-item">
+                    <span>Email:</span>
+                    <a href="mailto:skbansal907@gmail.com" className="mt-2 mb-3 underline">
+                      skbansal907@gmail.com
+                    </a>
+                  </div>
+                  <div class="link-item">
+                    <a href="https://www.instagram.com/defy.lifestyle/?hl=en" className='fs-2'>
+                      <IoLogoInstagram />
+                    </a>
+                    <a href="https://www.facebook.com/defylifestyle/" className='fs-2'>
+                      <IoLogoFacebook />
+                    </a>
+                  </div>
                 </div>
-              )}
-            </div>
-            <div className="col-12 col-md-3 ">
+              </div>
 
+            </div>
+
+
+            <div className="col-12 col-md-3">
               <br />
-              <div className="footer-link flex flex-col">
+
+              <div class="footer-link flex flex-col">
                 {isPolicyVisible && (
-                  <h4 className="mb-3" onClick={handlePolicyToggle}>
+                  <h4 class="mb-[1%]" onClick={handlePolicyToggle}>
                     Terms & Policies
                   </h4>
                 )}
-                {isPolicyOpen && (
-                  <>
-                    <Link className="py-2 mb-1" to="privacy-policy">
+
+                <div class={isPolicyOpen ? "link-container open" : "link-container"}>
+                  <div class="link-item">
+                    <Link class="py-2 mb-1" to="privacy-policy">
                       Privacy Policy
                     </Link>
-                    <Link className="py-2 mb-1" to="refund-policy">
+                  </div>
+                  <div class="link-item">
+                    <Link class="py-2 mb-1" to="refund-policy">
                       Refund Policy
                     </Link>
-                    <Link className="py-2 mb-1" to="shipping-policy">
+                  </div>
+                  <div class="link-item">
+                    <Link class="py-2 mb-1" to="shipping-policy">
                       Shipping Policy
                     </Link>
-                    <Link className="py-2 mb-1" to="terms-conditions">
+                  </div>
+                  <div class="link-item">
+                    <Link class="py-2 mb-1" to="terms-conditions">
                       Terms & Conditions
                     </Link>
-                  </>
-                )}
+                  </div>
+                </div>
               </div>
+
+
 
             </div>
             <div className="col-12 col-md-3">
               <br />
-              <div className="footer-link flex flex-col">
+
+              <div class="footer-link flex flex-col">
                 {isLinkVisible && (
-                  <h4 className="mb-3" onClick={handleLinkToggle}>
+                  <h4 class="mb-[1%]" onClick={handleLinkToggle}>
                     Links
                   </h4>
                 )}
 
-                {isLinkOpen && (
-                  <>
-                    <Link className="py-2 mb-1" to="/about-us">
+                <div class={isLinkOpen ? "link-container open" : "link-container"}>
+                  <div class="link-item">
+                    <Link class="py-2 mb-1" to="/about-us">
                       About Us
                     </Link>
-                    <Link to="/my-orders" className="py-2 mb-1">
+                  </div>
+                  <div class="link-item">
+                    <Link class="py-2 mb-1" to="/my-orders">
                       My Orders
                     </Link>
-                    <Link className="py-2 mb-1" to="/contact">
+                  </div>
+                  <div class="link-item">
+                    <Link class="py-2 mb-1" to="/contact">
                       Contact
                     </Link>
-                    <Link to="/wishlist" className="py-2 mb-1">
+                  </div>
+                  <div class="link-item">
+                    <Link class="py-2 mb-1" to="/wishlist">
                       My Wishlist
                     </Link>
-                  </>
-                )}
+                  </div>
+                </div>
               </div>
+
+
 
             </div>
           </div>
         </div>
       </footer>
-      <footer className='py-3 bg-white footer-bottom '>
+      <footer className='py-2 bg-white footer-bottom '>
         <div className="container-xxl">
           <div className="row">
             <div className="col-12">
-              <p className="text-center mb-8 mt-1 text-black">&copy; {new Date().getFullYear()} DEFY | All Rights Reserved </p>
+              <p className="text-center mb-2 mt-1 text-black">&copy; {new Date().getFullYear()} DEFY | All Rights Reserved </p>
             </div>
           </div>
         </div>
